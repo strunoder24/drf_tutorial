b868eefbf398493f5062b5e6647d882c03336a71
Виктор Кондик <strunoder24@mail.ru>
2019-05-19 16:35:13 +1000

    Конец 4 части

--- apps/snippets/views.py
+++ apps/snippets/views.py
@@ -1,15 +1,36 @@
 from rest_framework import status
 from rest_framework.response import Response
-from apps.snippets.models import Snippet
-from apps.snippets.serializers import SnippetSerializer
 from rest_framework import generics
+from rest_framework import permissions
+
+from apps.snippets.models import Snippet
+from django.contrib.auth.models import User
+
+from apps.snippets.serializers import UserSerializer
+from apps.snippets.serializers import SnippetSerializer
+from apps.snippets.permissions import IsOwnerOrReadOnly
 
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
--- project/urls.py
+++ project/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.snippets.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
